ddc37bf0e20cf8dbeaf01e6123070462ebc401bc
markovojinovic <77206213+markovojinovic@users.noreply.github.com>
2023-07-06 22:29:41 +0200

    Gotov CityOperations

--- src/rs/etf/sab/student/vm190559_CityOperations.java
+++ src/rs/etf/sab/student/vm190559_CityOperations.java
@@ -3,6 +3,10 @@ package rs.etf.sab.student;
 import rs.etf.sab.operations.CityOperations;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class vm190559_CityOperations implements CityOperations {
@@ -10,27 +14,141 @@ public class vm190559_CityOperations implements CityOperations {
     private static Connection connection = DB.getInstance().getConnection();
 
     @Override
-    public int createCity(String s) {
-        return 0;
+    public int createCity(String name) {
+
+        String query = "insert into Grad (Ime) values(?)";
+
+        try (PreparedStatement ps = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            ps.setString(1, name);
+
+            ps.execute();
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
     }
 
     @Override
     public List<Integer> getCities() {
-        return null;
+
+        List<Integer> ret = new ArrayList<>();
+
+        String query = "select IdGrad from Grad";
+
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                ret.add(rs.getInt(1));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
     }
 
     @Override
-    public int connectCities(int i, int i1, int i2) {
-        return 0;
+    public int connectCities(int cityId1, int cityId2, int distance) {
+
+        if (cityId1 == cityId2)
+            return -1;
+
+        String checkQuery = "select * from Saobracajnica where Grad1 = ? and Grad2 = ?";
+
+        try (PreparedStatement psCheck = connection.prepareStatement(checkQuery)) {
+            psCheck.setInt(1, cityId1);
+            psCheck.setInt(2, cityId2);
+
+            ResultSet checkRs1 = psCheck.executeQuery();
+            if (!checkRs1.next()) {
+                try (PreparedStatement psSecond = connection.prepareStatement(checkQuery)) {
+
+                    psSecond.setInt(1, cityId1);
+                    psSecond.setInt(2, cityId2);
+
+                    ResultSet checkRs2 = psSecond.executeQuery();
+                    if (!checkRs2.next()) {
+                        String insertQuery = "insert into Saobracajnica (Udaljenost, Grad1, Grad2) values (?, ?, ?)";
+                        try (PreparedStatement psInsert = connection.prepareStatement(insertQuery, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+                            psInsert.setInt(1, distance);
+                            psInsert.setInt(2, cityId1);
+                            psInsert.setInt(3, cityId2);
+
+                            psInsert.execute();
+                            ResultSet returnRs = psInsert.getGeneratedKeys();
+
+                            return returnRs.getInt(1);
+
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
     }
 
     @Override
-    public List<Integer> getConnectedCities(int i) {
-        return null;
+    public List<Integer> getConnectedCities(int cityId) {
+        List<Integer> ret = new ArrayList<>();
+
+        String query = "select IdGrad1 from Saobracajnica where IdGrad2 = ? " +
+                "union select IdGrad2 from Saobracajnica where IdGrad1 = ?";
+
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+
+            ps.setInt(1, cityId);
+            ps.setInt(2, cityId);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                ret.add(rs.getInt(1));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
     }
 
     @Override
-    public List<Integer> getShops(int i) {
-        return null;
+    public List<Integer> getShops(int cityId) {
+
+        List<Integer> ret = new ArrayList<>();
+
+        String query = "select IdProd from Prodavnice where IdGrad = ?";
+
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+
+            ps.setInt(1, cityId);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                ret.add(rs.getInt(1));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
     }
 }
